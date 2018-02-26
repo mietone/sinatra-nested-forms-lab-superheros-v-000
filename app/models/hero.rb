505060d64278c540e012b438a8bb26ef3ff9d3ad
Miwa <paintfx@gmail.com>
2018-02-26 18:35:13 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,3 +1,16 @@
-class Hero < ActiveRecord::Base
-  belongs_to :team
+class Hero
+  attr_reader :name, :power, :bio
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
